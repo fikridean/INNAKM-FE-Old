bd9b424d3fb9975e5e7e40a7ab9662c31646226c
fikridean <deanradityo@gmail.com>
2024-08-14 23:11:13 +0700

    feat: added new components

--- app/portals/[slug]/page.tsx
+++ app/portals/[slug]/page.tsx
@@ -8,9 +8,7 @@ export default function ShowPortalDetail(data: { params: { slug: string } }) {
     return (
       <div>
         <BackButton />
-
         <h1 className='mt-4'>Get Detail Portal</h1>
-
         <PortalDetail slug={slug} />
       </div>
     );
--- app/portals/page.tsx
+++ app/portals/page.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import PortalList from "./PortalList";
+import InputModal from '../components/InputModal';
 
 export default function AllPortals() {
   try {
     return (
       <div>
+        {/* <InputModal /> */}
         <h1>All Portals</h1>
-
         <PortalList />
       </div>
     );
+
   } catch (error) {
     return (
       <div>
